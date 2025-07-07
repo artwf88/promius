a2be12efb0491272e825521f6f12eace19bb0c96
artwf88 <bratiy9888@gmail.com>
2025-07-07 17:31:44 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,14 +11,14 @@ function fcFrameMeta(): Plugin {
     transformIndexHtml(html: string) {
       const config = {
         version: 'next',
-        imageUrl: 'TUNNEL/logo.png',
+        imageUrl: 'promius.vercel.app/logo.png',
         button: {
           title: '🚩 Start',
           action: {
             type: 'launch_frame',
             name: 'HelloWorld',
-            url: `TUNNEL/?v=${appVersion}`, 
-            splashImageUrl: 'TUNNEL/logo.png'
+            url: `promius.vercel.app/?v=${appVersion}`, 
+            splashImageUrl: 'promius.vercel.app/logo.png'
           },
         },
       };
@@ -37,7 +37,7 @@ export default defineConfig({
   server: {
     port: 3000,
     allowedHosts: [
-      '5abe318fed171fa07244be15c4e93881.serveo.net'
+      'promius.vercel.app'
     ],
     headers: {
       'X-Frame-Options': 'ALLOWALL',
